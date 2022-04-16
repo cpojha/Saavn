c0b286c7a3f54242e012279e3063a397182b92ce
ShashankKumarSaxena <shashanknetsol@gmail.com>
2022-04-16 17:46:01 +0530

    wtf lol

---
+++ src/main/java/com/saavn/Launcher.java
@@ -0,0 +1,11 @@
+package com.saavn;
+
+import com.saavn.core.SaavnDna;
+
+import javax.security.auth.login.LoginException;
+
+public class Launcher {
+    public static void main(String args[]) throws LoginException {
+        new SaavnDna();
+    }
+}
