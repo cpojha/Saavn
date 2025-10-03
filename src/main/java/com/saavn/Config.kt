0cd8fe6e78f821cefd8c8ec596fc16fa590ba954
Shashank Saxena <69804741+ShashankKumarSaxena@users.noreply.github.com>
2025-10-03 14:22:31 +0530

    Update Config.kt

--- src/main/java/com/saavn/Config.kt
+++ src/main/java/com/saavn/Config.kt
@@ -4,6 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv
 
 class Config {
     companion object {
+        // Make a .env file to load all the configs
         val dotenv = Dotenv.load()
 
         fun get(key: String) : String? {
